ac20d8f815ed421954bd87c4e657216feb5e2164
eunsookim1 <92eunsoo@gmail.com>
2023-06-14 21:23:29 -0700

    addProperty new function refactored

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -54,7 +54,7 @@ const getUserWithId = (id) => {
 
 const addUser = (user) => {
   return pool.query(`
-  INSERT INTO users(name, email, password)
+  INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3) RETURNING *
   `, [user.name, user.email, user.password]).
     then(res => {
@@ -192,11 +192,52 @@ const filter = (queryParams) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+const addProperty = (property) => {
+  const queryString = `
+  INSERT INTO properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    active
+  )
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;`;
+
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    true
+    // why is it like this?
+  ];
+  
+  return pool.query(queryString, queryParams)
+    .then((res) => {
+      console.log(res.rows[0]);
+      return res.rows[0];
+    });
 };
 
 module.exports = {
